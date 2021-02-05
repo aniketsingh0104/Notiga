0ceb900f37d8aaa8a36cfab8a80368c153bae662
Aniket Singh <aniketsingh0104@gmail.com>
2021-02-05 09:18:14 +0530

    Fix elevation, padding. Add truncate long text.

--- app/src/main/java/com/dullabs/notiga/ui/components/NotificationComponent.kt
+++ app/src/main/java/com/dullabs/notiga/ui/components/NotificationComponent.kt
@@ -10,38 +10,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dullabs.notiga.R
+import com.dullabs.notiga.ui.theme.NotigaTheme
 
 @Composable
 fun NotificationComponent() {
     Card(Modifier
             .fillMaxWidth()
-            .padding(10.dp),elevation = 8.dp) {
+            .padding(10.dp),elevation = 4.dp) {
         Row(Modifier
                 .fillMaxWidth()
-                .padding(10.dp), verticalAlignment = Alignment.CenterVertically) {
+                .padding(20.dp), verticalAlignment = Alignment.CenterVertically) {
             Image(imageVector = vectorResource(id = R.drawable.ic_whatsapp))
             Spacer(Modifier.width(12.dp))
-            Column() {
-                Row() {
+            Column {
+                Row {
                     Text("Chrome", style = MaterialTheme.typography.subtitle2)
                     Spacer(Modifier.width(40.dp))
-                    Text(text = "+2", style = MaterialTheme.typography.caption)
+                    Text(text = "+2", color = MaterialTheme.colors.secondaryVariant, style = MaterialTheme.typography.caption)
                 }
-                Text("We have some.", style = MaterialTheme.typography.overline)
+                Text("We have some crap that you want to check out.", modifier = Modifier.width(150.dp), style = MaterialTheme.typography.overline, overflow = TextOverflow.Ellipsis, maxLines = 1)
             }
             Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
                 Icon(imageVector = vectorResource(id = R.drawable.ic_arrow_right))
+
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewNotification() {
-    NotificationComponent()
+    NotigaTheme {
+        NotificationComponent()
+    }
 }
 
