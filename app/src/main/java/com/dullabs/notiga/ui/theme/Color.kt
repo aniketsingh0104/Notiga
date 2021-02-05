8fe1bf162d979b097f8634e0e00c096c971217c1
Aniket Singh <aniketsingh0104@gmail.com>
2021-02-05 08:58:35 +0530

    Fix color codes.

--- app/src/main/java/com/dullabs/notiga/ui/theme/Color.kt
+++ app/src/main/java/com/dullabs/notiga/ui/theme/Color.kt
@@ -2,7 +2,18 @@ package com.dullabs.notiga.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val blue700 = Color(0xff1E2232)
+val blue500 = Color(0xff212A3D)
+val blue400 = Color(0xff283349)
+val blue200 = Color(0xff5873DB)
+val blue100 = Color(0xFFADBDF7)
+
+val grey700 = Color(0xff343745)
+val grey600 = Color(0xff363947)
+val grey500 = Color(0xff454B61)
+val grey300 = Color(0x80A1A1A1)
+
+val red200 = Color(0xffF68167)
+val green400 = Color(0xff4CAF50)
+val white600 = Color(0xCCFFFFFF)
+val white300 = Color(0x80FFFFFF)
