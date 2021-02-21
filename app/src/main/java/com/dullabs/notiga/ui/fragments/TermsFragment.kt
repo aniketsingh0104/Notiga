49f34855bd1abfe00a2b898e5b83c8055aec7c25
Aniket Singh <aniketsingh0104@gmail.com>
2021-02-21 11:59:01 +0530

    Add on attach, on detach

--- app/src/main/java/com/dullabs/notiga/ui/fragments/TermsFragment.kt
+++ app/src/main/java/com/dullabs/notiga/ui/fragments/TermsFragment.kt
@@ -1,19 +1,45 @@
 package com.dullabs.notiga.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dullabs.notiga.R
+import com.dullabs.notiga.MainActivity
+import com.dullabs.notiga.databinding.FragmentTermsBinding
 
 class TermsFragment : Fragment() {
+
+    private var _binding: FragmentTermsBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-//        (activity as MainActivity).setupNavigationIcon()
-        return inflater.inflate(R.layout.fragment_apps_configure, container, false)
+    ): View {
+        _binding = FragmentTermsBinding.inflate(inflater, container, false)
+        return _binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        (activity as MainActivity).hideBottomBar()
+        (activity as MainActivity).hideFab()
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        (activity as MainActivity).showBottomBar()
+        (activity as MainActivity).showFab()
+        super.onDetach()
     }
 }
